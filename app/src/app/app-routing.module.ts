59e2dddc4061f46ea7817fa5ea870464167755a9
Anass Benfillous <anasse.benfillous@gmail.com>
2025-02-27 12:01:06 +0000

    add a 'Site Under Construction' page

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WarningComponent } from './pages/warning/warning.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: WarningComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
