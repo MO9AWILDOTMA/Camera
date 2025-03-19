638b3758876b7e344c0623faca310949b30b06a5
Anass Benfillous <anasse.benfillous@gmail.com>
2025-03-19 12:45:37 +0000

    fixing missing props

--- public-app/src/app/home/our-stats.tsx
+++ public-app/src/app/home/our-stats.tsx
@@ -26,16 +26,23 @@ export function OurStats() {
   return (
     <section className="container mx-auto grid gap-10 px-8 py-36 lg:grid-cols-1 lg:gap-20 xl:grid-cols-2 xl:place-items-center">
       <div>
-        <Typography variant="h6" color="orange" className="mb-6 font-medium">
+        <Typography
+          {...({} as any)}
+          variant="h6"
+          color="orange"
+          className="mb-6 font-medium"
+        >
           Our Stats
         </Typography>
         <Typography
+          {...({} as any)}
           className="text-5xl font-bold leading-tight lg:w-3/4"
           color="blue-gray"
         >
           Historical Significance
         </Typography>
         <Typography
+          {...({} as any)}
           variant="lead"
           className="mt-3 w-full !text-gray-500 lg:w-9/12"
         >
