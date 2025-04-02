6c98e3df16835e14bd81b3411c5d08f93d3b3c61
Anass Benfillous <anasse.benfillous@gmail.com>
2025-04-02 07:34:23 +0000

    fix prod output

--- public-app/next.config.js
+++ public-app/next.config.js
@@ -1,11 +1,12 @@
 const nextConfig = {
+  output: "export", // Enable static HTML export
   images: {
     unoptimized: true, // Required for static export
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "https", // Change to https for production
-        hostname: "https://cinecamera-api-c39c6ec4c5e9.herokuapp.com", // Update to your production API domain
+        hostname: "cinecamera-api-c39c6ec4c5e9.herokuapp.com", // Update to your production API domain
         port: "",
         pathname: "/images/**",
       },
