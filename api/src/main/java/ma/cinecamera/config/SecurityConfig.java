1c742b31587cd3d85b7dfebd6861151490381b2a
Anass Benfillous <anasse.benfillous@gmail.com>
2025-04-02 08:00:12 +0000

    fix cors config

--- api/src/main/java/ma/cinecamera/config/SecurityConfig.java
+++ api/src/main/java/ma/cinecamera/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 	CorsConfiguration configuration = new CorsConfiguration();
-	configuration.setAllowedOrigins(Arrays.asList("http://localhost:3001", "https://camerapublic.vercel.app/", "http://localhost:3000/"));
+	configuration.setAllowedOrigins(Arrays.asList("http://localhost:3001", "https://www.cinecamera.ma", "http://localhost:3000/"));
 	configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 	configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept",
 		"Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
